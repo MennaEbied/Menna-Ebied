5bff773b61aa52ddf6839ada707b8fd0cd47a3df
MennaEbied <mennahesham253@gmail.com>
2025-08-25 06:15:13 +0300

    updates

--- assets/custom-sections.js
+++ assets/custom-sections.js
@@ -67,7 +67,7 @@ document.addEventListener('DOMContentLoaded',async function(){
                      <select class="size-select"><option value="">choose size</option></select>
                 </div>
             </div>
-            <button class="add-to-cart-btn">Add to cart <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="svg-icon">
+            <button class="add-to-cart-btn">Add to cart <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="cart-svg-icon">
             <path d="M5 12H19M19 12L12 19M19 12L12 5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
           </svg></button>
         </div>

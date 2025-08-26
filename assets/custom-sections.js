5ab67d470173d0843e50a3d9c7957c5360a7a5ca
MennaEbied <mennahesham253@gmail.com>
2025-08-26 05:24:27 +0300

    updates

--- assets/custom-sections.js
+++ assets/custom-sections.js
@@ -63,9 +63,11 @@ document.addEventListener('DOMContentLoaded', async function() {
             <div class="sizes-dropdown">
               <div class="selected">
                 <label class="placeholder">Choose your size</label>
+                <div class="arrow-container">
                 <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="dropdown-arrow-icon">
                     <path d="M19 9L12 16L5 9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
+                </div>
               </div>
               <ul class="dropdown-options"></ul>
             </div>
